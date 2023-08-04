3634a828d220e124c562c689537ae001781a612a
ReeperC3 <cian3neiland@gmail.com>
2023-08-04 12:26:48 +0100

    Edited Rock_paper_scissors to use the DOM.

--- script.js
+++ script.js
@@ -4,80 +4,81 @@ let computerScore = 0;
 function computerChoice() {
   let aiChoice = Math.floor(Math.random() * 3);
   return aiChoice;
-}
+} // 0 is rock, 1 is paper, 2 is scissors
 
+const choiseRock = document.querySelector('.rock')
+const choisePaper = document.querySelector('.paper')
+const choiseScissors = document.querySelector('.scissors')
+const result = document.querySelector('.result');
+const score = document.querySelector('.score')
+const reset = document.querySelector('.reset')
+const winner = document.querySelector('.winner')
 
-
-
-
-
-// 0 is rock, 1 is paper, 2 is scissors
-
-  
-
-
-function play() {
-  let playerSelection = getPlayerSelection();
-  let computerSelection = computerChoice();
-
-  function whoWon(playerSelected, computerSelected) {
-  if (playerSelected === "rock" && computerSelected === 2) {
-    playerScore++;
-    return "Congratulations! Rock beats scissors.";
-  } else if (playerSelected === "rock" && computerSelected === 0) {
-    return "It is a draw. Rock and rock.";
-  } else if (playerSelected === "rock" && computerSelected === 1) {
-    computerScore++;
-    return "Unlucky. Paper beats your rock.";
-  } else if (playerSelected === "paper" && computerSelected === 0) {
-    playerScore++;
-    return "Congratulations! Paper beats rock.";
-  } else if (playerSelected === "paper" && computerSelected === 1) {
-    return "It is a draw. Paper and paper.";
-  } else if (playerSelected === "paper" && computerSelected === 2) {
-    computerScore++
-    return "Unlucky. Scissors beats your paper.";
-  } else if (playerSelected === "scissors" && computerSelected === 2) {
-    playerScore++;
-    return "Congratulations! Scissors beats paper.";
-  } else if (playerSelected === "scissors" && computerSelected === 0) {
-    return "It is a draw. Scissors and scissors.";
-  } else if (playerSelected === "scissors" && computerSelected === 1) {
-    computerScore++;
-    return "Unlucky. Rock beats your scissors.";
-  } else {
-    return "That is not a valid input. You must enter rock, paper, or scissors."
+choiseRock.addEventListener("click", function () {
+  if (computerChoice() === 0) {
+    result.textContent = "DRAW";
+  } if (computerChoice() === 1) {
+    result.textContent = "LOSE";
+    computerScore += 1;
+  } if (computerChoice() === 2) {
+    result.textContent = "WIN";
+    playerScore += 1;
   }
-} 
-  
-  
-  console.log(whoWon(playerSelection, computerSelection));
-  console.log(`Player: ${playerScore}. Computer: ${computerScore}`);
-}
-
-for (let i = 0; i < 5; i++) {
-  let playerChoice = prompt("Please enter: Rock, paper or scissors?");
-  function getPlayerSelection() {
-    if (playerChoice.toLowerCase() === "rock") {
-      return "rock";
-    } else if (playerChoice.toLowerCase() === "paper") {
-      return "paper";
-    } else if (playerChoice.toLowerCase() === "scissors") {
-      return "scissors";
-    } else {
-      return "invalid"
-    }
+  score.innerHTML = `YOU: ${playerScore}. COMPUTER: ${computerScore}.`
+  if (playerScore === 5) {
+    winner.innerHTML = "You won! Play again?";
   }
-  play();
-  if (playerScore === 3) {
-    console.log("You won! refresh the page to play again.")
-    break;
-  } else if (computerScore === 3) {
-    console.log("You lost. Refresh the page to play again.")
-    break;
-  } 
-  
-}
+  if (computerScore === 5) {
+    winner.innerHTML = "You lost. Play again?";
+  }
+}); 
+choisePaper.addEventListener("click", 
+function () {
+  if (computerChoice() === 0) {
+    result.textContent = "WIN";
+    playerScore += 1;
+  } if (computerChoice() === 1) {
+    result.textContent = "DRAW";
+  } if (computerChoice() === 2) {
+    result.textContent = "LOSE";
+    computerScore += 1;
+  }
+  score.innerHTML = `YOU: ${playerScore}. COMPUTER: ${computerScore}.`
+  if (playerScore === 5) {
+    winner.innerHTML = "You won! Play again?";
+  }
+  if (computerScore === 5) {
+    winner.innerHTML = "You lost. Play again?";
+  }
+});
+choiseScissors.addEventListener("click", 
+function () {
+  if (computerChoice() === 0) {
+    result.textContent = "LOSE";
+    computerScore += 1;
+  } if (computerChoice() === 1) {
+    result.textContent = "WIN";
+    playerScore += 1;
+  } if (computerChoice() === 2) {
+    result.textContent = "DRAW";
+  }
+  score.innerHTML = `YOU: ${playerScore}. COMPUTER: ${computerScore}.`
+  if (playerScore === 5) {
+    winner.innerHTML = "You won! Play again?";
+  }
+  if (computerScore === 5) {
+    winner.innerHTML = "You lost. Play again?";
+  }
+});
+reset.addEventListener("click", function () {
+  playerScore = 0;
+  computerScore = 0;
+  winner.innerHTML = "";
+  result.innerHTML = "";
+  score.innerHTML = `YOU: ${playerScore}. COMPUTER: ${computerScore}.`;
+});
+
+
 
 
 
